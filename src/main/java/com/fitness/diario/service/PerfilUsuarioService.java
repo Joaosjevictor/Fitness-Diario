ad7b389a9a9a2ab7ada695e79b13a5155a97c42b
Adson Ruan <121983752+adruan12@users.noreply.github.com>
2026-06-01 21:05:27 -0300

    Atualização do CRUD

--- src/main/java/com/fitness/diario/service/PerfilUsuarioService.java
+++ src/main/java/com/fitness/diario/service/PerfilUsuarioService.java
@@ -18,19 +18,54 @@ public class PerfilUsuarioService {
     @Autowired
     public PerfilUsuarioService(PerfilUsuarioRepository repository, List<ObjetivoStrategy> strategyList) {
         this.repository = repository;
+<<<<<<< HEAD
+
+=======
         // Transforma a lista de estratégias em um Mapa para busca super rápida (O(1))
+>>>>>>> 1a1069683e19badb36346655baba389fe39f3f87
         this.strategies = strategyList.stream()
                 .collect(Collectors.toMap(ObjetivoStrategy::getObjetivo, s -> s));
     }
 
     public PerfilUsuario criarPerfilEGerarPlano(PerfilUsuario usuario) {
+<<<<<<< HEAD
+
+        ObjetivoStrategy strategy = strategies.get(usuario.getObjetivo());
+
+=======
         // 1. Busca a estratégia correta baseada no objetivo do usuário
         ObjetivoStrategy strategy = strategies.get(usuario.getObjetivo());
         
+>>>>>>> 1a1069683e19badb36346655baba389fe39f3f87
         if (strategy == null) {
             throw new IllegalArgumentException("Objetivo não suportado: " + usuario.getObjetivo());
         }
 
+<<<<<<< HEAD
+        strategy.montarPlano(usuario);
+
+        return repository.save(usuario);
+    }
+
+    public List<PerfilUsuario> buscarPorObjetivo(String objetivo) {
+        return repository.findByObjetivo(objetivo);
+    }
+
+    // NOVOS MÉTODOS
+
+    public List<PerfilUsuario> listarTodos() {
+        return repository.findAll();
+    }
+
+    public PerfilUsuario atualizarPerfil(PerfilUsuario usuario) {
+        return repository.save(usuario);
+    }
+
+    public void deletarPerfil(String id) {
+        repository.deleteById(id);
+    }
+}
+=======
         // 2. Aplica a regra de negócio comportamental
         strategy.montarPlano(usuario);
 
@@ -43,3 +78,4 @@ public class PerfilUsuarioService {
         return repository.findByObjetivo(objetivo);
     }
 }
+>>>>>>> 1a1069683e19badb36346655baba389fe39f3f87
