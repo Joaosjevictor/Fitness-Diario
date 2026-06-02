c0e92b69a4d03e3453db10407cc06660006fe2f6
Adson Ruan <121983752+adruan12@users.noreply.github.com>
2026-06-01 21:06:17 -0300

    Atualização do CRUD

--- src/main/java/com/fitness/diario/model/PerfilUsuario.java
+++ src/main/java/com/fitness/diario/model/PerfilUsuario.java
@@ -7,12 +7,20 @@ import org.springframework.data.couchbase.core.mapping.id.GeneratedValue;
 import org.springframework.data.couchbase.core.mapping.id.GenerationStrategy;
 import org.springframework.data.couchbase.repository.Collection;
 import lombok.Data;
+<<<<<<< HEAD
+
+=======
+>>>>>>> 1a1069683e19badb36346655baba389fe39f3f87
 import java.util.List;
 import java.util.Map;
 
 @Data
 @Document
+<<<<<<< HEAD
+@Collection("perfis")
+=======
 @Collection("perfis") // Onde os documentos serão salvos dentro do Bucket
+>>>>>>> 1a1069683e19badb36346655baba389fe39f3f87
 public class PerfilUsuario {
 
     @Id
@@ -25,6 +33,21 @@ public class PerfilUsuario {
     @Field
     private double pesoAtual;
 
+<<<<<<< HEAD
+    // NOVO CAMPO PARA DEMONSTRAR ESTRUTURA DOCUMENTAL
+    @Field
+    private List<Double> historicoPesos;
+
+    @Field
+    private String objetivo;
+
+    @Field
+    private List<String> restricoesMedicas;
+
+    @Field
+    private Map<String, PlanoDiario> planoSemanal;
+}
+=======
     @Field
     private String objetivo; // Ex: "Hipertrofia", "Emagrecimento"
 
@@ -35,3 +58,4 @@ public class PerfilUsuario {
     @Field
     private Map<String, PlanoDiario> planoSemanal; 
 }
+>>>>>>> 1a1069683e19badb36346655baba389fe39f3f87
